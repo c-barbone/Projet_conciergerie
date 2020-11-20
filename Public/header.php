812976ef4dc244a48dc8538e5834d24cfeabffc1
c.barbone <c.barbone@codeur.online>
2020-11-20 18:11:36 +0100

    script font awesome manquant

--- Public/header.php
+++ Public/header.php
@@ -20,6 +20,8 @@
     </script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/d69db61c7b.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
